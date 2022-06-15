ffed0dbd96ce7087278a7fb3604cc8ae7ec45133
Joshua McLean <mrjoshuamclean@gmail.com>
2022-06-14 20:25:53 -0400

    Clean up main js, add branding page

--- main.js
+++ main.js
@@ -4,14 +4,12 @@ function addJam(jam, where) {
         const inner = `${jam.name}`;
         const link = `<a href='${url}' target='_blank'>${inner}</a>`;
         const image_link = `<a href='${url}' target='_blank'>${image}</a>`;
-        //const para = `<div class='jam'>${image}</div>`;
         const para = `<div class='jam'>${image_link}<div class='jam-link'>${link}</div></div>`;
         where.innerHTML += `${para}`;
 }
 
 $.get(
         "get.php",
-        {/*paramOne : 1, paramX : 'abc'*/},
         function(data) {
                 const json_data = JSON.parse(data);
                 const where = document.getElementById('jam-grid');
