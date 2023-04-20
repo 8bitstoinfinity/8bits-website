a1df2f73e64323f5f9bd91346f5c0360ce98e7fa
Joshua McLean <mrjoshuamclean@gmail.com>
2023-04-20 22:00:01 +0000

    Add missing html tag

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
-<head>
-        <title>8 Bits to Infinity</title>
-        <link rel="stylesheet" href="styles.css" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
-<body>
-        <header id="navbar">
-                <a href="#">
-                        <img id="main-logo" src="img/8bits-logo-sideways.png" />
-                </a>
-                <?php include "inc/social-media.html" ?>
-                <?php include "inc/toc.html" ?>
-        </header>
-        <main class="content">
-                <?php include "inc/jams.html" ?>
-                <?php include "inc/mission.html" ?>
-                <?php include "inc/jam-rules.html" ?>
-                <?php include "inc/jam-faq.php" ?>
-                <?php include "inc/diversifiers.html" ?>
-                <?php include "inc/rating-guide.html" ?>
-                <?php include "inc/branding.html" ?>
-                <?php /*include "inc/jamming1.php"*/ ?>
-        </main>
-        <footer>
-                <?php include "inc/footer.html" ?>
-        </footer>
-<script src="main.js"></script>
-</body>
+<html>
+	<head>
+		<title>8 Bits to Infinity</title>
+		<link rel="stylesheet" href="styles.css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" defer></script>
+		<script src="main.js" defer></script>
+	</head>
+	<body>
+		<header id="navbar">
+			<a href="#">
+				<img id="main-logo" src="img/8bits-logo-sideways.png" />
+			</a>
+			<?php /*include "inc/social-media.html"*/ ?>
+			<?php /*include "inc/toc.html"*/ ?>
+		</header>
+		<main class="content">
+			<?php /*include "inc/jams.html"*/ ?>
+			<?php /*include "inc/mission.html"*/ ?>
+			<?php /*include "inc/jam-rules.html"*/ ?>
+			<?php /*include "inc/jam-faq.php"*/ ?>
+			<?php /*include "inc/diversifiers.html"*/ ?>
+			<?php /*include "inc/rating-guide.html"*/ ?>
+			<?php /*include "inc/branding.html"*/ ?>
+			<!--don't include this one! !!-->
+			<?php /*include "inc/jamming1.php"*/ ?>
+		</main>
+		<footer>
+			<?php /*include "inc/footer.html"*/ ?>
+		</footer>
+	</body>
 </html>
