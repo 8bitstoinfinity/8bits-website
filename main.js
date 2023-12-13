f5a649f471edd05ff99fc10850cd06cc9ef8a2e5
Retro Indie Josh <mrjoshuamclean@gmail.com>
2023-12-13 17:00:09 -0500

    Add numbering and split Shape Jam parts

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
+count = 41;
+
 function addJam(jam, where) {
-        const url = `${jam.url}/results`;
-        const image = `<img class='jam-logo' src='${jam.logo}' />`
-        const inner = `${jam.name}`;
-        const link = `<a href='${url}' target='_blank'>${inner}</a>`;
-        const image_link = `<a href='${url}' target='_blank'>${image}</a>`;
-        const para = `<div class='jam'>${image_link}<div class='jam-link'>${link}</div></div>`;
-        where.innerHTML += `${para}`;
+    const url = `${jam.url}/results`;
+    const image = `<img class='jam-logo' src='${jam.logo}' />`
+    const inner = `${jam.name}`;
+    const link = `<a href='${url}' target='_blank'>${inner}</a>`;
+    const image_link = `<a href='${url}' target='_blank'>${image}</a>`;
+    const para = `<div class='jam'>${image_link}<div class='jam-link'>#${count}<br />${link}</div></div>`;
+    where.innerHTML += `${para}`;
+    count -= 1;
 }
 
 $.get(
-        "get.php",
-        function(data) {
-                const json_data = JSON.parse(data);
-                const where = document.getElementById('jam-grid');
-                json_data.jams.slice().reverse().forEach(jam => addJam(jam, where));
-        }
+    "get.php",
+    function (data) {
+        const json_data = JSON.parse(data);
+        const where = document.getElementById('jam-grid');
+        json_data.jams.slice().reverse().forEach(jam => addJam(jam, where));
+    }
 );
